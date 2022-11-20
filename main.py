f827199937041a2e3b878b1eceb1bcc4927f0c7b
El Samouel <samuel.cardoso@ede.esiee-it.fr>
2022-11-20 16:58:57 +0100

    Corrections du menu

--- main.py
+++ main.py
@@ -93,7 +93,6 @@ class Game:
     def main_menu(self):
         while True:
             # Main menu FULLSCREEN
-            self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
             w, h = pygame.display.get_surface().get_size()
             self.background = pygame.transform.scale(self.background, (w, h))
 
@@ -102,13 +101,13 @@ class Game:
             MENU_MOUSE_POS = pygame.mouse.get_pos()
 
             MENU_TEXT = self.get_font(100).render("MAIN MENU", True, "#b68f40")
-            MENU_RECT = MENU_TEXT.get_rect(center=(w/2, 400))
+            MENU_RECT = MENU_TEXT.get_rect(center=(w/2, 300))
 
-            PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(w/2, 700), 
+            PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(w/2, 500), 
                                 text_input="PLAY", font=self.get_font(75), base_color="#d7fcd4", hovering_color="White")
-            OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(w/2, 850), 
+            OPTIONS_BUTTON = Button(image=pygame.image.load("assets/Options Rect.png"), pos=(w/2, 650), 
                                 text_input="OPTIONS", font=self.get_font(75), base_color="#d7fcd4", hovering_color="White")
-            QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(w/2, 1000), 
+            QUIT_BUTTON = Button(image=pygame.image.load("assets/Quit Rect.png"), pos=(w/2, 800), 
                                 text_input="QUIT", font=self.get_font(75), base_color="#d7fcd4", hovering_color="White")
 
             self.screen.blit(MENU_TEXT, MENU_RECT)
